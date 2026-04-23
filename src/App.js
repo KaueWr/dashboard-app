4e08ccb428f434ccf553a1d2ae139ddc662f63a5
Your Name <you@example.com>
2026-04-23 09:11:33 -0300

    Fix: Forçando atualização da URL do Apps Script

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ const COLORS = {
 const GOOGLE_SHEET_LINK = 'https://docs.google.com/spreadsheets/d/1urzSyHpTI9QvP3xG7k4OmocVEaTKcaDjuwat1xdN38Y/edit?usp=sharing';
 
 // ⭐ URL DO GOOGLE APPS SCRIPT AQUI (VOCÊ VAI COLOCAR DEPOIS DE PUBLICAR O SCRIPT)
-const GOOGLE_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwf0lzYHdvyr6XVIuB8LE72Ckjc7IiSPZNbomCmFTs5OnKMJfmBnnlpTTen6sKu47z6zQ/exec'; 
+const GOOGLE_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyn5xr7cPZm1D8cib7KKL6C4o3Gv1S8LeoC1z1IfHgXtkYK0u6FnK5dCrHHww9GWIbCbw/exec'; 
 
 function App() {
   const [data, setData] = useState(null);
